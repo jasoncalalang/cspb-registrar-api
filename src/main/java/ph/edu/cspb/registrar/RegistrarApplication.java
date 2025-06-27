36fba492a5b4461a096f9f247a8e3c3108c4927d
Jason Calalang <jcalalang@ph.ibm.com>
2025-06-27 23:50:27 +0800

    Initial commit

---
+++ src/main/java/ph/edu/cspb/registrar/RegistrarApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.cspb.registrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ph/edu/cspb/registrar/RegistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.cspb.registrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
