be760fbaf029839939e3850598a06acc3d2b038d
Jason Calalang <jcalalang@ph.ibm.com>
2025-06-28 12:30:21 +0800

    update

--- src/test/java/ph/edu/cspb/registrar/StudentControllerIT.java
+++ src/test/java/ph/edu/cspb/registrar/StudentControllerIT.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
