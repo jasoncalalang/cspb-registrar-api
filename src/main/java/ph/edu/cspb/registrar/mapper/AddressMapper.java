6bd1ad7f6c96565e5b4d8d6ca10566691bfac8a5
Jason Calalang <jcalalang@ph.ibm.com>
2025-06-28 14:21:01 +0800

    fix errors

--- src/main/java/ph/edu/cspb/registrar/mapper/AddressMapper.java
+++ src/main/java/ph/edu/cspb/registrar/mapper/AddressMapper.java
@@ -3,9 +3,14 @@ package ph.edu.cspb.registrar.mapper;
 import ph.edu.cspb.registrar.dto.AddressDto;
 import ph.edu.cspb.registrar.model.Address;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
     AddressDto toDto(Address address);
+
+    @Mapping(target = "studentId", ignore = true)
+    @Mapping(target = "student", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     Address toEntity(AddressDto dto);
 }
--- src/main/java/ph/edu/cspb/registrar/mapper/StudentMapper.java
+++ src/main/java/ph/edu/cspb/registrar/mapper/StudentMapper.java
@@ -3,9 +3,13 @@ package ph.edu.cspb.registrar.mapper;
 import ph.edu.cspb.registrar.dto.StudentDto;
 import ph.edu.cspb.registrar.model.Student;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
     StudentDto toDto(Student student);
+
+    @Mapping(target = "address", ignore = true)
+    @Mapping(target = "requirements", ignore = true)
     Student toEntity(StudentDto dto);
 }
