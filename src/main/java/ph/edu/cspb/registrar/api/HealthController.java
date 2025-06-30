c549396b84f35acdfc3fdfd308c985a866a60da5
Jason Calalang <jcalalang@ph.ibm.com>
2025-06-30 15:24:55 +0800

    adjust health controller url

--- src/main/java/ph/edu/cspb/registrar/api/HealthController.java
+++ src/main/java/ph/edu/cspb/registrar/api/HealthController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @RestController
 public class HealthController {
 
-    @GetMapping({"/health", "/api/health"})
+    @GetMapping({"/api/health"})
     public ResponseEntity<Map<String, String>> health() {
         return ResponseEntity.ok(Map.of("message", "successful"));
     }
